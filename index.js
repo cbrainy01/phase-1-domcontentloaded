93c6813a0279f61668d452a8ed46b9da03964c35
Carl <carlbrainegebe@gmail.com>
2021-05-26 12:17:39 -0500

    commit tree 69c965f31f917aa40397fc0eca813d9946044bbf

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", replaceText);
+
+function replaceText(event) {
+const paragraph = document.querySelector("p#text");
+paragraph.textContent = "This is really cool!";
+}
